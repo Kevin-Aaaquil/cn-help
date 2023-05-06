b692d8b5b49c95bfc60fac21ebb4ab0a85e071e1
Kevin-Aaaquil <eshansingh2409@gmail.com>
2023-05-06 19:38:46 +0530

    dynamic

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,9 @@ import fs from "fs";
     const data = fs.readFileSync("./files/help.txt", "utf8");
     res.send(data);
   });
+  const port = process.env.PORT || 3000;
 
-  app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  app.listen(port, () => {
+    console.log("Server started on port :"+port);
   });
 })();
