83b62eeaa7c4d4aba6fb5164742da061e9c5993b
Kevin-Aaaquil <eshansingh2409@gmail.com>
2023-05-06 19:21:25 +0530

    fresh start

--- files/arpClient.c
+++
@@ -1,8 +0,0 @@
-import socket
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('localhost',6968))
-add = input('Enter IP: ')
-s.send(add.encode())
-mac = s.recv(1024)
-mac = mac.decode('utf-8')
-print('MAC of',add,' is: ',mac)
